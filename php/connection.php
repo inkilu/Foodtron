080907e9579c4aee6c1b01219392c821dc56d701
inkilu <gauthamgkm@gmail.com>
2020-09-26 19:12:56 +0530

    connected to foodtron Db

--- php/connection.php
+++ php/connection.php
@@ -1,6 +1,17 @@
 <?php
 
-$dbname=;
-$
+$dbname="foodtron_db";
+$dbusername="root";
+$dbpassword="";
+$dbhost="localhost";
+$conn = mysqli_connect($dbhost,$dbusername,$dbpassword,$dbname);
+if($conn){
+echo "Success";
+}
+
+else{
+    echo "Not Success" + mysqli_error();
+}
+
 
 ?>
