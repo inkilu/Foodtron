630cdbc1b1f980456d5a61a00e09ea4f73e54830
inkilu <gauthamgkm@gmail.com>
2020-10-05 21:02:45 +0530

    Minor Change

--- php/connection.php
+++ php/connection.php
@@ -5,13 +5,13 @@ $dbusername="root";
 $dbpassword="";
 $dbhost="localhost";
 $conn = mysqli_connect($dbhost,$dbusername,$dbpassword,$dbname);
-if($conn){
+/*if($conn){
 echo "Success";
 }
 
 else{
     echo "Not Success" + mysqli_error($sonn);
 }
-
+*/
 
 ?>
