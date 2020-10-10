828f37bc47a8f289d1d521d5fb4c927e17314c65
inkilu <gauthamgkm@gmail.com>
2020-10-10 15:51:02 +0530

    Added Sessions

--- php/login.php
+++ php/login.php
@@ -5,12 +5,12 @@ if(isset($_POST['usernmeoremail'])){
     
    $uname=$_POST['usernmeoremail'];
    $pswrd=$_POST['passwrdlog'];
-  $_SESSION['sess']=$uname;
+  
    $sql = "select * from registration where username='".$uname."'AND password='".$pswrd."' limit 1";
    $result=mysqli_query($conn,$sql);
   
    if(mysqli_num_rows($result)== 1){
-  
+    $_SESSION['sess']=$uname;
     echo " You Have Successfully Logged in";
     exit();
 }
