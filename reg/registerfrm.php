3cb634aff9c78bcd0da968b076089b9b910dcef2
inkilu <gauthamgkm@gmail.com>
2020-11-18 22:37:10 +0530

    Placeholder change

--- reg/registerfrm.php
+++ reg/registerfrm.php
@@ -25,22 +25,22 @@
                         <form method="POST" class="register-form" id="register-form" action="../php/register.php">
                             <div class="form-group">
                                 <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="reg_name" id="name" placeholder="Your Name"/>
+                                <input type="text" name="reg_name" id="name" placeholder="Username" required/>
                             </div>
                             <div class="form-group">
                                 <label for="email"><i class="zmdi zmdi-email"></i></label>
-                                <input type="email" name="email_" id="email" placeholder="Your Email"/>
+                                <input type="email" name="email_" id="email" placeholder="Your Email" required/>
                             </div>
                             <div class="form-group">
                                 <label for="pass"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="pass_" id="pass" placeholder="Password"/>
+                                <input type="password" name="pass_" id="pass" placeholder="Password" required/>
                             </div>
                             <div class="form-group">
                                 <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
-                                <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password"/>
+                                <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password" required/>
                             </div>
                             <div class="form-group">
-                                <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
+                                <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" required/>
                                 <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
                             </div>
                             <div class="form-group form-button">
